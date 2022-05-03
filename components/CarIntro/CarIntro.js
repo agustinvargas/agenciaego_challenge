5eb921ea7f9b61d0dfc092544c6632361a732b5f
Agustín Vargas <agustinvargas93@gmail.com>
2022-05-03 13:17:32 -0300

    refactor CarIntro and Highlights

--- components/CarIntro/CarIntro.js
+++ components/CarIntro/CarIntro.js
@@ -4,18 +4,18 @@ import parse from 'html-react-parser';
 
 const CarIntro = ({ model }) => {
   return (
-    <div className='max-w-[1139px] m-auto lg:flex-row lg:flex lg:justify-between lg:items-center'>
-      <div className='relative grow w-[345px] h-[194px] lg:w-[559px] lg:h-[320px]'>
+    <div className='lg:max-w-[1139px] md:max-w-[650px] m-auto px-[15px] lg:px-[0px] lg:flex-row lg:flex lg:justify-between lg:items-center'>
+      <div className='relative grow w-[100%] h-[194px] md:h-[320px]'>
         <Image
           src={model.photo}
           alt={model.name}
           layout='fill'
-          className='object-cover lg:object-contain'
+          className='object-contain lg:object-contain'
         />
       </div>
-      <div className='mt-[42px] mb-[48px]'>
+      <div className='mt-[42px] md:mt-[0]'>
         <span className='text-[20px] lg:w-[475px] font-semibold block mb-[14px] text-[#373737] leading-[1.35]'>
-          {model.name} {model.segment}
+          {model.name}
         </span>
         <h1 className='text-[#191919] tracking-[-0.7px] leading-[1.26] font-semibold mb-[19px] text-[35px] lg:text-[50px] lg:mb-[37px] lg:tracking-[-1px] lg:leading-[1.14] lg:text-[#373737] lg:w-[475px]'>
           {model.title}
--- components/Highlights/Highlights.js
+++ components/Highlights/Highlights.js
@@ -6,17 +6,18 @@ const Highlights = ({ model }) => {
   return model?.model_highlights.map((highlight, i) => (
     <div
       key={i}
-      className='max-w-[1139px] m-auto flex flex-col gap-[21px] mt-[40px] lg:mb-[55px] lg:flex-row lg:items-center lg:justify-between odd:lg:flex-row-reverse odd:lg:pl-[90px] even:lg:pr-[90px]'
+      className='lg:max-w-[1139px] px-[15px] md:px-[0px] m-auto flex flex-col gap-[21px] mt-[40px] first:mt-[0] md:mb-[55px] md:flex-row md:items-center md:justify-between odd:md:flex-row-reverse odd:md:pl-[90px] even:md:pr-[90px]'
     >
-      <div className='relative w-[345px] h-[194px] lg:w-[559px] lg:h-[296px]'>
+      <div className='relative w-[100%] h-[194px] md:w-[559px] md:h-[296px]'>
         <Image
           src={highlight.image}
           alt={highlight.title}
           layout='fill'
-          className='object-cover lg:object-contain'
+          className='object-cover md:object-contain'
+          priority
         />
       </div>
-      <div className='lg:w-[385px]'>
+      <div className='md:w-[385px]'>
         <h3 className='text-[#373737] text-[20px] tracking-[-0.4px] leading-[1.35] font-semibold mb-[20px]'>
           {highlight.title}
         </h3>
