214950cfa3fee1262e1513b6d41a516a360d5648
Agustín Vargas <agustinvargas93@gmail.com>
2022-05-03 13:16:53 -0300

    refactor header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MenuHamb } from '../Icons/MenuHamb';
 import Logo from '../Logo/Logo';
 import MenuOffcanvas from '../MenuOffcanvas/MenuOffcanvas';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const Header = () => {
   const [openOffCanvas, setOpenOffCanvas] = useState(false);
+  const { pathname } = useRouter();
 
   const handleMenu = () => {
     document.querySelector('body').classList.toggle('overflow-hidden');
@@ -13,15 +16,40 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <div
-        id='header'
-        className='flex flex-row justify-between items-center p-[16px] lg:px-[35px] lg:py-[16px] h-[70px] border-b-[1px] border-[#d8d8d8] border-solid'
-      >
-        <Logo />
-        <MenuHamb handleMenu={handleMenu} />
+    <header id='header'>
+      <div className='flex flex-row justify-between items-center h-[70px] lg:px-[35px] px-[15px] border-b-[1px] border-[#d8d8d8] border-solid'>
+        <div className='flex items-center gap-[77.46px]'>
+          <Logo />
+          <nav className='hidden lg:inline-block cursor-pointer'>
+            <ul className='flex gap-[40.63px]'>
+              <li
+                className={`${
+                  pathname === '/modelos' &&
+                  'border-b-[red] border-b-[4px] text-[#D0021B]'
+                } block text-center font-semibold py-[22px]`}
+              >
+                <Link href='/modelos'>
+                  <a className='px-[32px]'>Modelos</a>
+                </Link>
+              </li>
+              <li
+                className={`${
+                  pathname !== '/modelos'
+                    ? 'border-b-[red] border-b-[4px] text-[#D0021B]'
+                    : 'hidden'
+                } block text-center font-semibold border-b-[red] border-b-[4px] py-[22px] min-qw-[126px]`}
+              >
+                <span className='px-[32px]'>Ficha de modelo</span>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className='flex items-center gap-[12px]'>
+          <span className='hidden md:inline-block text-[14px]'>Menú</span>
+          <MenuHamb handleMenu={handleMenu} />
+        </div>
       </div>
-      <MenuOffcanvas handleMenu={handleMenu} />
+      <MenuOffcanvas handleMenu={handleMenu} openOffCanvas={openOffCanvas} />
       {/* By default, is showed in DOM (SEO)  */}
     </header>
   );
